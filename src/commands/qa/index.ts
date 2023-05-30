05262e539c48d920c967d0d5cd844002cb6eaaa0
Oliver Klingefjord <oliverklingefjord@gmail.com>
2023-05-30 17:23:16 +0200

    lil refactor

--- src/commands/qa/index.ts
+++ src/commands/qa/index.ts
@@ -1,27 +1,32 @@
-import { Context } from 'grammy'
-import Mendable from '../../core/mendable'
+import { Context } from "grammy"
+import Mendable from "../../core/mendable"
 
 const qa = async (ctx: Context): Promise<void> => {
+  let response: string | undefined
+
     // Send a typing task until the api call is done.
-    let taskDone = false
-    ;(async function sendTyping() {
-        while (!taskDone) {
-            await ctx.replyWithChatAction('typing')
-            await new Promise(resolve => setTimeout(resolve, 5000))
-        }
-    })()
+  ;(async function sendTyping() {
+    while (!response) {
+      await ctx.replyWithChatAction("typing")
+      await new Promise((resolve) => setTimeout(resolve, 5000))
+    }
+  })()
 
-    await ctx.replyWithChatAction('typing')
+  await ctx.replyWithChatAction("typing")
 
-    const mendable = new Mendable(String(process.env.MENDABLE_API_KEY))
-    const response = await mendable.call(ctx)
+  const mendable = new Mendable(String(process.env.MENDABLE_API_KEY))
 
-    taskDone = true
+  try {
+    response = await mendable.call(ctx)
+  } catch (err) {
+    console.error(err)
+    response = "I'm sorry, I didn't understand that."
+  }
 
-    await ctx.reply(response, {
-        parse_mode: 'Markdown',
-        disable_web_page_preview: true,
-    })
+  await ctx.reply(response, {
+    parse_mode: "Markdown",
+    disable_web_page_preview: true,
+  })
 }
 
 export default qa
--- src/core/mendable.ts
+++ src/core/mendable.ts
@@ -1,75 +1,67 @@
-import { Context } from 'grammy'
-import fetch from 'node-fetch'
+import { Context } from "grammy"
+import fetch from "node-fetch"
 
 class Mendable {
-    private api_key: string
-    private conversationId?: string
+  private apiKey: string
+  private conversationId?: string
 
-    constructor(api_key: string) {
-        this.api_key = api_key
+  constructor(api_key: string) {
+    this.apiKey = api_key
+  }
+
+  private async _start_conversation(): Promise<string> {
+    const data = { api_key: this.apiKey }
+
+    const response = await fetch("https://api.mendable.ai/v0/newConversation", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    })
+
+    const response_data = await response.json()
+    return response_data["conversation_id"]
+  }
+
+  public async call(ctx: Context): Promise<string> {
+    const query = ctx.message?.text
+
+    if (!query) {
+      return "I'm sorry, I didn't understand that."
     }
 
-    private async _start_conversation(): Promise<string> {
-        const data = { api_key: this.api_key }
-
-        console.log('Getting conversation id with api key: ', this.api_key)
-
-        const response = await fetch(
-            'https://api.mendable.ai/v0/newConversation',
-            {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers: { 'Content-Type': 'application/json' },
-            }
-        )
-
-        const response_data = await response.json()
-        return response_data['conversation_id']
+    if (!this.conversationId) {
+      this.conversationId = await this._start_conversation()
     }
 
-    public async call(ctx: Context): Promise<string> {
-        const query = ctx.message?.text
+    // Send another chat action in case the api call took some time.
+    console.log("Sending chat action: typing")
+    await ctx.replyWithChatAction("typing")
 
-        if (!query) {
-            return "I'm sorry, I didn't understand that."
-        }
-
-        if (!this.conversationId) {
-            this.conversationId = await this._start_conversation()
-        }
-
-        // Send another chat action in case the api call took some time.
-        console.log('Sending chat action: typing')
-        await ctx.replyWithChatAction('typing')
-
-        const data = {
-            question: query,
-            shouldStream: false,
-            conversation_id: this.conversationId,
-            history: [],
-            api_key: this.api_key,
-        }
-
-        const response = await fetch(
-            'https://api.mendable.ai/v0/mendableChat',
-            {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers: { 'Content-Type': 'application/json' },
-            }
-        )
-
-        const response_data = await response.json()
-        let response_text = response_data['answer']['text']
-        const sources = response_data['sources'].slice(0, 5) // first five sources
-
-        if (sources.length > 0) {
-            response_text += '\n\n*Sources:*\n'
-            response_text += sources.map(s => s['link']).join('\n')
-        }
-
-        return response_text
+    const data = {
+      question: query,
+      shouldStream: false,
+      conversation_id: this.conversationId,
+      history: [],
+      api_key: this.apiKey,
     }
+
+    const response = await fetch("https://api.mendable.ai/v0/mendableChat", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    })
+
+    const response_data = await response.json()
+    let response_text = response_data["answer"]["text"]
+    const sources = response_data["sources"].slice(0, 5) // first five sources
+
+    if (sources.length > 0) {
+      response_text += "\n\n*Sources:*\n"
+      response_text += sources.map((s) => s["link"]).join("\n")
+    }
+
+    return response_text
+  }
 }
 
 export default Mendable
