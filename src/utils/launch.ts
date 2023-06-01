cb52cbe8785c0a4b9930c68a0b4e5439d67736c6
Oliver Klingefjord <oliverklingefjord@gmail.com>
2023-06-01 11:42:43 +0200

    set up webhook

--- src/utils/launch.ts
+++ src/utils/launch.ts
@@ -1,8 +1,12 @@
 import { Bot } from "grammy"
+import fetch from "node-fetch"
 import { MyContext } from "src/types"
 
 const production = async (bot: Bot<MyContext>): Promise<void> => {
   try {
+    await fetch(
+      `https://api.telegram.org/bot${bot.token}/setWebhook?url=${process.env.VERCEL_URL}`
+    )
     await bot.api.setWebhook(`${process.env.VERCEL_URL}`)
     console.log(`[SERVER] Bot starting webhook`)
   } catch (e) {
